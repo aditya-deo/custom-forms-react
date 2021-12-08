84cd9afde9a08dfe2ede2798db562e1465c1edf3
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2021-12-08 15:25:16 +0530

    add styling and resolve bugs

--- src/Components/FormElement/FormElement.js
+++ src/Components/FormElement/FormElement.js
@@ -1,20 +1,21 @@
 import React from "react";
+import "./FormElement.css";
 
 const FormElement = (props) => {
   if (props.statusCode === 1) {
-    return <input type="email" placeholder="E-mail"></input>;
+    return <input type="email" className="formElement__email" placeholder="E-mail"></input>;
   } 
   else if (props.statusCode === 2) {
-    return <input type="text" placeholder="Enter Text"></input>;
+    return <input type="text" className="formElement__singleLineInput" placeholder="Enter Text"></input>;
   } 
   else if (props.statusCode === 3) {
-    return <textarea></textarea>
+    return <textarea placeholder="Text Area" className="formElement__textArea"></textarea>
   } 
   else if (props.statusCode === 4) {
-    return <input type="password" placeholder="Enter Password"></input>;
+    return <input type="password" className="formElement__password" placeholder="Enter Password"></input>;
   } 
   else if (props.statusCode === 5) {
-    return <button type="submit">Submit</button>
+    return <button type="submit" className="formElement__submitButton">Submit</button>
   } 
   else {
     return <></>;
